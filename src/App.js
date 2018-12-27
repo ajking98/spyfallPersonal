f0b6842e0e876d1a39db2772f7d27bbc6fd6901c
Ahmed <agedi98@gmail.com>
2018-12-27 11:55:54 -0500

    Upload react app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './components/Form.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './components/Home';
+import BeforeGame from './components/BeforeGame';
+import PreparePlayers from './components/PreparePlayers';
+import Game from './components/Game';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="main">
+          <Route exact path='/' component={Home} />
+            <Route exact path='/BeforeGame' component={BeforeGame} />
+            <Route exact path='/PreparePlayers' component={PreparePlayers} />
+            <Route exact path='/AccessCodePlaceholder' component={Game} />
+        </div>
+      </Router>
     );
   }
 }
